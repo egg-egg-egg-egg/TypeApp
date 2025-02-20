76a78be2808abd0b7bfe30bfe619e7d962bf17fd
unknown <1127885315@qq.com>
2025-02-21 00:50:19 +0800

    添加了字典目录dicts

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 这是最初的程序流程图，随着程序的不断完善和丰富，流程图也会不断更新。
 
 ![typeApp流程图](./assets/typeApp.png)
+
+
+# 感谢
+
+本项目的字典数据都是来自[qwerty-learner](https://github.com/RealKai42/qwerty-learner)项目，感谢作者和各路伙伴的开源。
--- typeApp/main.cpp
+++ typeApp/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <json.hpp>
+#include <string>
+#include <fstream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+using json = nlohmann::json; 
 
-int main(int argc, char** argv) {
-	printf("hello");
+int main() {
+//	string jsonstr = "{\"name\": \"Tom\", \"age\": 30}";
+//	json data = json::parse(jsonstr);
+//	
+//	cout<<" 姓名: "<< data["name"] << "\n年龄: "<<data["age"];
+    
+	
+//	"data.json"
+	ifstream file("data.json");
+	if(file){
+		json file_data;
+		file >> file_data;
+		cout<<"文件内容是: "<<file_data["name"];
+	}else{
+		cerr << "文件打开失败";
+	}
 	return 0;
 }
